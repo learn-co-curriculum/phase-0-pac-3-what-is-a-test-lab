b093637d7c93abb058c706d919534b23778f49d4
Liz Burton <liz.burton147@gmail.com>
2020-12-21 15:46:13 -0500

    update to es6

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,4 @@
-var index = require("../index.js")
+const index = require("../index.js");
 
 describe('what-is-a-test', () => {
   describe('Name', () => {
@@ -20,6 +20,4 @@ describe('what-is-a-test', () => {
       expect(index.message).toInclude(index.height)
     })
   })
-
 })
-
