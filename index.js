c15fab953cb9800b25c135598876571af0d72d57
Remberto Villar <rembertovillar@gmail.com>
2022-06-10 10:14:16 -0400

    commit tree efc0ade0595a35745dabac15892c95fab46c477a

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 38;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+console.log(message);
