7bf1ac084bc418b988ca36d79585aa6d7a5969a3
DavidWolff218 <dcwolff218@gmail.com>
2023-06-02 12:26:46 -0500

    updated test file to account for greater than 0

--- test/indexTest.js
+++ test/indexTest.js
@@ -10,7 +10,8 @@ describe("what-is-a-test", () => {
   });
 
   describe("Height", () => {
-    it("is less than 40", () => {
+    it("is less than 40 and greater than 0", () => {
+      expect(height).toBeMoreThan(0)
       expect(height).toBeLessThan(40);
     });
   });
